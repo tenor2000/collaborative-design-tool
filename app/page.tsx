b0de0bc26c9de5cecd5f573fa041af49d6c563ed
tenor2000 <tenor2000@gmail.com>
2024-10-10 13:04:55 -0400

    created navbar and users

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,16 @@
 "use client"
 
 import Live from "@/components/Live";
+import Navbar from "@/components/navbar";
 
 export default function Page() {
   return (
-    <div >
-      <Live />
-    </div>
+    <main className="h-screen overflow-hidden">
+      <Navbar />
+      <section className="flex h-full flex-row">
+        <Live />
+      </section>
+      
+    </main>
   );
 }
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'liveblocks.io',
+          port: '',
+          pathname: '/avatars/**',
+        },
+      ],
+    },
+  };
+  
+  export default nextConfig;
+  
